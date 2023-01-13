e28b1b1ee69a7ef3de3eb4d3b8af47ef6d8e0538
YueMelody <106568602+YueMelody@users.noreply.github.com>
2023-01-13 09:14:56 -0500

    Delete doc directory

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BookTest"},{"p":"<Unnamed>","l":"PersonTest"},{"p":"<Unnamed>","l":"SquareTest"}];updateSearchResults();
